2d02f581522161151da3d11f44bf0ad27736242b
Ian Chan <icha024@gmail.com>
2018-12-03 19:09:47 +0000

    Friendly view

--- src/main.js
+++ src/main.js
@@ -2,16 +2,23 @@
 new Vue({
   el: '#app',
   data: {
-    message: '(Try BTC or ETH)',
-    currency: ''
+    price: '',
+    priceByCurrency: {
+      USD: '',
+      EUR: '',
+      GBP: '',
+    },
+    currency: '',
   },
   methods: {
     fetchData: function () { 
         var vm = this
         axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=' + vm.currency + '&tsyms=USD,EUR,GBP').then(response => {
           if (vm.currency.length == 3) {
-            vm.message = response.data
-            console.debug('Got: ' + vm.message)
+            vm.price = response.data
+            vm.priceByCurrency.USD = response.data[vm.currency].USD
+            vm.priceByCurrency.EUR = response.data[vm.currency].EUR
+            vm.priceByCurrency.GBP = response.data[vm.currency].GBP
           }
         })
     }
