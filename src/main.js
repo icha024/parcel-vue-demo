5e10158defc705982aec13f5a68cd465b0da1e10
Ian Chan <icha024@gmail.com>
2018-12-03 18:45:49 +0000

    Query crypto gold service

--- src/main.js
+++ src/main.js
@@ -2,5 +2,14 @@ new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue.js!'
+  },
+  methods: {
+    fetchData: function () { 
+        var vm = this
+        axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD,EUR').then(response => {
+          vm.message = response.data
+          console.debug('Got: ' + vm.message)
+        })
+    }
   }
 })
